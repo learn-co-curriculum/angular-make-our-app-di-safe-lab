e357ee2a532cfc5963060ed81e8820e550fc6c3b
Todd Motto <todd@toddmotto.com>
2016-02-26 00:22:53 +0000

    Fixed typo

--- tests/ContactController.spec.js
+++ tests/ContactController.spec.js
@@ -1,5 +1,5 @@
 // The annotate function below takes in a function, converts it into a string, parses out the argument names and adds them to an array.
-// This is very similiar to how Angular does dependency injection by argument name: cheeckout this blog post for more information! https://toddmotto.com/angular-js-dependency-injection-annotation-process/
+// This is very similiar to how Angular does dependency injection by argument name: checkout this blog post for more information! https://toddmotto.com/angular-js-dependency-injection-annotation-process/
 
 var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m;
 var FN_ARG_SPLIT = /,/;
